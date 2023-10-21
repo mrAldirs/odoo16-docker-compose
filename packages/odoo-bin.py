b7bb53c1f0b2cfb6720dc65cec39f42cf0a42117
mrAldirs <mr.aldirs@yahoo.com>
2023-10-21 08:02:28 +0700

    mrAldirs

--- packages/odoo-bin.py
+++ packages/odoo-bin.py
@@ -1,12 +1,21 @@
+# ---------------------------------------
+# | Install Odoo 16 with docker-compose |
+# ---------------------------------------
+# To initiate the installation of Odoo-16.0, execute the following Docker-Compose command in your terminal:
+# docker-compose up -d
+#
+# If necessary, you can perform a restart of Odoo using the following command:
+# docker-compose restart
+#
+# To gracefully conclude the installation, use the following command to stop and terminate the services:
+# docker-compose down
+
 # -----------------------
 # | Add Python packages |
 # -----------------------
 # To install below packages at startup, uncomment this line in "docker-compose.yml" file!
 # - ./entrypoint.sh:/entrypoint.sh
-
-# -----------------------------
-# | Install addons (optional) |
-# -----------------------------
+#
 # pip install pip --upgrade
 # 
 # * if you want to install enterprise addons
